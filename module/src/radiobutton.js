ac1de20be77b12767e555afd1a76852110895cd1
Mikael Tellhed <mikael.tellhed@gmail.com>
2021-01-21 16:21:24 +0100

    Radio button groups work

--- module/src/radiobutton.js
+++ module/src/radiobutton.js
@@ -1,56 +1,30 @@
 import Noodl from '@noodl/noodl-sdk';
 import Utils from './utils';
-import Events from 'events';
+import RadioButtonContext from './radiobuttoncontext';
 
 import {
 	useEffect,
-	useState,
+    useState,
+    useContext,
 } from 'react';
-
-const stateEmitter = new Events.EventEmitter();
 
 // --------------------------------------------------------------------------------------
 // RadioButton
 // --------------------------------------------------------------------------------------
 function RadioButton(props) {
-  //  const [checked, setChecked] = useState(props.checked);
-
-    var _currentRadioButtonGroup;
-    function _radioButtonChanged(ev) {
-        props.checkedChanged && props.checkedChanged(ev.value === props.value)
-    }
+    const radioButtonGroup = useContext(RadioButtonContext)
 
     // On mount
 	useEffect(() => {
-	//	setChecked(!!props.checked);
-   //     props.checkedChanged && props.checkedChanged(!!props.checked);
-
-        props.checkedChanged && props.checkedChanged(false);
         props.focusChanged && props.focusChanged(false);
         props.hoverChanged && props.hoverChanged(false);
-
-        // Listen to changes in radio button group
-        stateEmitter.on(props.name,_radioButtonChanged)
-        _currentRadioButtonGroup = props.name;
     }, []);
-    
-    // Register listener if name changed
-    useEffect(() => {
-        if(_currentRadioButtonGroup !== undefined) stateEmitter.off(_currentRadioButtonGroup,_radioButtonChanged)
-        stateEmitter.on(props.name,_radioButtonChanged)
-        _currentRadioButtonGroup = props.name;
-    }, [props.name]);
 
-/*	useEffect(() => {
-		setChecked(props.checked);
-        props.checkedChanged && props.checkedChanged(props.checked);
-	}, [props.checked]);*/
-
-    return <input className="ndl-formkit-radiobutton" type="radio" {...props} disabled={!props.enabled} 
+    props.checkedChanged && props.checkedChanged(radioButtonGroup.selected === props.value);
+    return <input className="ndl-formkit-radiobutton" type="radio" name={radioButtonGroup.name} {...props} disabled={!props.enabled} 
+        checked={radioButtonGroup.selected === props.value}
         onChange = {e => {
-            stateEmitter.emit(props.name,{value:props.value})
-       //     setChecked(e.target.checked);
-       //     props.checkedChanged && props.checkedChanged(e.target.checked);
+            radioButtonGroup.checkedChanged && radioButtonGroup.checkedChanged(props.value);
         }}
 		onFocus={e => props.enabled && props.focusChanged && props.focusChanged(true)}
         onBlur={e => props.enabled && props.focusChanged && props.focusChanged(false)}
@@ -67,6 +41,11 @@ var RadioButtonNode = {
         this.props.sizeMode = 'explicit';
         this.props.id = this.outputs.controlId = 'input-' + Utils.guid();
         this.props.enabled = this.outputs.enabled = true;
+        this.props.checkedChanged = (value) => {
+            this.setOutputs({
+                checked:value
+            })
+        }
 	},
 	getReactComponent() {
 		return RadioButton;
@@ -90,10 +69,10 @@ var RadioButtonNode = {
     outputs:{
         controlId:{type:'string',displayName:'Control Id',group:'General'},
         enabled: {type:'boolean', displayName:'Enabled',group:'States'},
+        checked: {type:'boolean', displayName:'Checked',group:'States'},
     },
 	inputProps: {
       //  checked: {type:'boolean', displayName:'Checked',group:'General'},
-        name: {type:'string', displayName:'Name',group:'General'},
         value: {type:'string', displayName:'Value',group:'General'},
 		width: {
 			index: 11,
@@ -119,7 +98,7 @@ var RadioButtonNode = {
 		}
 	},
 	outputProps: {
-		checkedChanged: {type: 'boolean', displayName: 'Checked', group:'States'},
+		//checkedChanged: {type: 'boolean', displayName: 'Checked', group:'States'},
         focusChanged: {type: 'boolean', displayName: 'Focused', group:'States'},
         hoverChanged: {type: 'boolean', displayName: 'Hover', group:'States'}
 	}
